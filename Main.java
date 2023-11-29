6926ed6b68cf7a1aba3c5260fc409e294ffc3a5a
Kalana Indula <kalanasmedia@outlook.com>
2023-11-29 11:48:40 +0530

    First_Commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+
+        int myValue=10000;
+
+        // int a=3;
+        // int b=4;
+        // int z=a+b-1;
+        // System.out.println(z);
+
+        int myMinValue=Integer.MIN_VALUE;
+        int myMaxValue=Integer.MAX_VALUE;
+
+        // System.out.println(myValue);
+        System.out.println("Minimum value can be held by a integer = "+myMinValue);
+        System.out.println("Maximum value can be held by a integer = "+myMaxValue);
+
+
+        float myMinFloatValue=Float.MIN_VALUE;
+        float myMaxFloatValue=Float.MAX_VALUE;
+
+        System.out.println("Minimum value can be held by a float = "+myMinFloatValue);
+        System.out.println("Maximum value can be held by a float = "+myMaxFloatValue);
+
+        // int nonDecimelNumber=1000;
+        // short  a=16;
+        // byte b=2;
+        // long l=100L;
+
+        // double d=100.2;
+        // float f=102.4f;
+
+        // char myChar='5';
+        // String myString="Awetgwweg";       
+        
+    }
+}
